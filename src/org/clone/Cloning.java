d19b4cc918c68ba72b52265114b17db5d277a397
unknown <vickywaran43@gmail.com>
2022-04-03 13:41:54 +0530

    commit

---
+++ src/org/clone/Cloning.java
@@ -0,0 +1,13 @@
+package org.clone;
+
+public class Cloning {
+	public static void main(String[] args) {
+		int num = 14;
+		if (num % 2 == 0) {
+			System.out.println("even number");
+		} else {
+			System.out.println("odd number");
+		}
+	}
+
+}
